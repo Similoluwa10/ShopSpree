f713d97e2e652595ca665e51f13399df6545f663
Similoluwa <similoluwagbenle04@gmail.com>
2025-03-16 18:22:03 +0100

    added debug-False for production

--- Shopspree/settings.py
+++ Shopspree/settings.py
@@ -40,10 +40,8 @@ DATABASES = {
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-
-# ALLOWED_HOSTS = ['shopspree-two.vercel.app', 'localhost', '127.0.0.1']
 
 ALLOWED_HOSTS = ["*"]
 
